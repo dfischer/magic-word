5f49104f1b59587929e5fb4fa2a73d42c10aca8d
Matthew Blount <xkapastel@gmail.com>
2019-01-04 12:38:02 -0500

    Move block rendering to app directory.

---
+++ src/app/ui/Block.js
@@ -0,0 +1,22 @@
+// This file is a part of Denshi.
+// Copyright (C) 2019 Matthew Blount
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <https://www.gnu.org/licenses/.
+
+// Visualize a string of bytecode.  I'm thinking of things like
+// Clojure's [REBL](https://github.com/cognitect-labs/REBL-distro).
+export default function Block(props) {
+  return <p>Unimplemented.</p>
+}
