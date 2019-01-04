9516d13a3a4f645876ebd7d6eb10d424304fcac3
Matthew Blount <xkapastel@gmail.com>
2019-01-04 17:17:24 -0500

    Deploying on GitHub Pages/Netlify.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
 const path = require("path");
 
 let app = {
-  input: "./src/app/index.js",
+  entry: "./src/app/index.js",
   output: {
     path: path.resolve(__dirname, "lib"),
     filename: "app.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
 
 let bot = {
-  input: "./src/bot/index.js",
+  entry: "./src/bot/index.js",
   output: {
     path: path.resolve(__dirname, "bin"),
     filename: "bot.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
   },
 };
 
