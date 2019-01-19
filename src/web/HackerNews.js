5d4eda4fda66c00885de2b6ebcac6002d3060773
Matthew Blount <xkapastel@gmail.com>
2019-01-19 07:08:23 -0500

    Add license header to a stray file.

--- src/web/HackerNews.js
+++ src/web/HackerNews.js
@@ -1,3 +1,20 @@
+// This file is a part of Denshi.
+// Copyright (C) 2019 Matthew Blount
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <https://www.gnu.org/licenses/.
+
 import * as pup from "puppeteer";
 
 export default {
@@ -6,9 +23,10 @@ export default {
     const page = await browser.newPage();
     await page.goto("https://news.ycombinator.com");
     let links = await page.evaluate(() => {
-      return Array
-        .from(document.querySelectorAll(".storylink"))
-        .map(x => x.href);
+      var elements = document.querySelectorAll(".storylink");
+      var elements = Array.from(elements);
+      var links = elements.map(x => x.href);
+      return links;
     });
     await browser.close();
     return links;
