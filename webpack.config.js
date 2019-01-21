066b9439b92a563a0e69252f02e20e4bf8aea95a
Matthew Blount <xkapastel@gmail.com>
2019-01-21 18:10:44 +0000

    Preparing for a new API.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
-let client = {
+let v0_client = {
   mode: "development",
   target: "web",
-  entry: "./src/client/index.js",
+  entry: "./src/v0/client/index.js",
   output: {
     path: path.resolve(__dirname, "lib"),
     filename: "denshi.js",
@@ -28,10 +28,10 @@ fs.readdirSync("node_modules")
   .filter(x => [".bin"].indexOf(x) === -1)
   .forEach(x => externals[x] = `commonjs ${x}`);
 
-let server = {
+let v0_server = {
   mode: "development",
   target: "node",
-  entry: "./src/server/index.js",
+  entry: "./src/v0/server/index.js",
   output: {
     path: path.resolve(__dirname, "bin"),
     filename: "denshi.js",
@@ -39,4 +39,4 @@ let server = {
   externals: externals,
 };
 
-module.exports = [client, server];
+module.exports = [v0_client, v0_server];
