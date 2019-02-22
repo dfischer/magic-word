4483071054c1b70d431cd99d1a7e1898cfc5641d
Matthew Blount <xkapastel@gmail.com>
2019-02-22 21:54:11 +0000

    Move Option module to its own file.

---
+++ src/PlanetForth.Lang/Option.fs
@@ -0,0 +1,34 @@
+// This file is a part of Planet Forth.
+// Copyright (C) 2019 Matthew Blount
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <https://www.gnu.org/licenses/.
+
+namespace PlanetForth.Lang
+
+module Option =
+  let all (xs: 'a option list): 'a list option =
+    let cons (value: 'a option) (xs: 'a list): 'a list option =
+      match value with
+        | None   -> None
+        | Some x -> Some <| x :: xs
+
+    let step (state: 'a list option) (value: 'a option): 'a list option =
+      match state with
+        | None    -> None
+        | Some xs -> cons value xs
+
+    let init = Some []
+
+    List.fold step init <| List.rev xs
